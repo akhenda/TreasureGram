25644a6dd1e0b003916b98c4d6f57b77d92080ac
andela-akhenda <joseph.akhenda@andela.com>
2017-02-13 18:04:12 +0300

    [Chore] The Missing Model

--- main_app/views.py
+++ main_app/views.py
@@ -1,21 +1,8 @@
 from django.shortcuts import render
+from .models import Treasure
 
 
 # Create your views here.
 def index(request):
+    treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
-
-
-class Treasure:
-    def __init__(self, name, value, material, location, img_url):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-        self.img_url = img_url
-
-treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM", 'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-gold-nugget.png'),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Fall, CO", 'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-fools-gold.png'),
-    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA', 'https://s3.amazonaws.com/courseware.codeschool.com/try_django/images/treasuregram-coffee-can.png')
-]
